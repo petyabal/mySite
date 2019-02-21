c3b6b75c526e6f3ee610aa60ea91ad91668ed774
petyabal <petyabal@mail.ru>
2019-02-21 21:58:27 +0300

    mySite v.1.3.0

--- quiz/views.py
+++ quiz/views.py
@@ -11,7 +11,8 @@ def quizzes_list(request):
 
 def selected_quiz(request, pk):
 	quiz = get_object_or_404(Quiz, quiz_url=pk)
-	request.session["answers"] = 5# здесь устанавливаю первоначальное значение
+	request.session["questions"] = [] #здесь заводится список для хранения перечня вопросов
+	request.session["answers"] = []# список для хранения выбранных вариантов ответов 
 	return render(request, 'quiz/rules.html', {'quiz': quiz})
 
 def step_of_quiz(request, pk, step):
@@ -28,29 +29,28 @@ def step_of_quiz(request, pk, step):
 		all_steps = Step.objects.filter(step_of_quiz=pk).count()#получение числа шагов для progress bar
 		progress = str((my_step - 1) / all_steps * 100)#расчет прогресса прохождения
 		current = str(1 / all_steps * 100)
-		mylog = request.session["answers"]# вывод текущего значения
+		request.session["questions"] += str(question.id)
+		log = request.session["answers"]# вывод текущего значения
 		return render(request, 'quiz/question.html', {'quiz':quiz, 'step': step,# 
-			'question': question, 'answers': answer, 'next': next, 'mylog': mylog, 
+			'question': question, 'answers': answer, 'next': next, 'myLog': log, 
 			'progress': progress, 'current': current})#
 	else:
 		return redirect(result, pk)
 
-# изменение значения в зависимости от ответа
+# запоминание выбранного варианта ответа
 def quiz_log(request, pk, step, log):
-	mylog = log
-	if mylog == 1:
-		request.session["answers"] = 1
-	elif mylog  == 2:
-		request.session["answers"] = 2
-	elif mylog == 3:
-		request.session["answers"] = 3
-	elif mylog == 4:
-		request.session["answers"] = 4
+	request.session["answers"] += log
 	return redirect(step_of_quiz, pk, step)
 #
 
 def result(request, pk):
 	quiz = get_object_or_404(Quiz, quiz_url=pk)
-	mylog = request.session["answers"]# вывод конечного значения
-	del request.session["answers"]# удаление конечного значения
-	return render(request, 'quiz/result.html', {'quiz':quiz, 'mylog': mylog})
+	questionsLog = request.session["questions"]
+	answersLog = request.session["answers"]
+	qSet = Question.objects.filter(id__in=questionsLog).order_by('question_of_step__step_number')
+	aSet = Answer.objects.filter(answer_to_the_question__in=qSet).order_by(
+		'answer_to_the_question__question_of_step__step_number')
+	del request.session["questions"]# удаление списка вопросов
+	del request.session["answers"]# удаление списка ответов
+	return render(request, 'quiz/result.html', {'quiz':quiz, 'questions': questionsLog, 
+		'answers': answersLog, 'questionsSet': qSet, 'answersSet': aSet})
